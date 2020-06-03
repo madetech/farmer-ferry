4414dfc5ce385dd5bc42dfd51849623c1b7cdaa3
Will Gibson <Will Gibson>
2020-06-03 16:50:24 +0100

    Set meta title

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/farmer-ferry/'
-        : '/'
+        : '/',
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = 'CRAP'
+                return args
+            })
+    }
 }
