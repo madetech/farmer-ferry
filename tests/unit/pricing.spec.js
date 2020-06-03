ec9cd4cc0aa0379f8e97c106e6653b8685080e5b
Stu Mackellar <github@nusonic.co.uk>
2020-06-03 16:27:41 +0100

    Added more geese.

--- tests/unit/pricing.spec.js
+++ tests/unit/pricing.spec.js
@@ -2,15 +2,35 @@ import { calculatePrices } from '@/services/pricing.js';
 
 describe('pricing', () => {
 
-    it('3 bags @ 25 = 125', () => {
+    it('3 corn @ 25 = 125', () => {
         expect(calculatePrices(3, 0, 25)).toMatchObject( { price: 125 });
     })
 
-    it('0 bags', () => {
+    it('0 corn', () => {
         expect(calculatePrices(0, 0, 25)).toMatchObject( { price: 0 });
     })
 
-    it('invalid bags', () => {
+    it('invalid corn', () => {
         expect(calculatePrices('x', 0, 25)).toMatchObject({ error: "Please enter valid values"});
+    })
+
+    it('3 geese @ 25 = 125', () => {
+        expect(calculatePrices(0, 3, 25)).toMatchObject( { price: 125 });
+    })
+
+    it('0 geese @ 25 = 0', () => {
+        expect(calculatePrices(0, 0, 25)).toMatchObject( { price: 0 });
+    })
+
+    it('3 corn 3 geese @ 25 = error', () => {
+        expect(calculatePrices(3, 3, 25)).toMatchObject( { error: "Goose alert! Corn at risk!" });
+    })
+
+    it('2 corn 2 geese @ 25 = error', () => {
+        expect(calculatePrices(3, 3, 25)).toMatchObject( { error: "Goose alert! Corn at risk!" });
+    })
+
+    it('2 corn 1 geese @ 25 = 125', () => {
+        expect(calculatePrices(2, 1, 25)).toMatchObject( { price: 125 });
     })
 })
