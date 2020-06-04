c2d7ee555fb7c6b904f994d76c930d89f27535d0
Stu Mackellar <github@nusonic.co.uk>
2020-06-04 10:44:25 +0100

    Testing travel plan for no commodities.

--- src/services/travel-plan.js
+++ src/services/travel-plan.js
@@ -1,3 +1,5 @@
-export function generateTravelPlan() {
+export function generateTravelPlan(commodities) {
+    if (commodities.corn === 1) return { plan: ["corn"]}
+    
     return { plan: [ "goose" ] };
 }
--- tests/unit/travel-plan.spec.js
+++ tests/unit/travel-plan.spec.js
@@ -9,10 +9,14 @@ import { generateTravelPlan } from '@/services/travel-plan.js';
 describe('travel plan', () => {
 
     test('that a single goose returns a travel plan showing a single crossing with the farmer and a goose', () => {
-        expect(generateTravelPlan(0, 1, 25)).toMatchObject( { plan: [ "goose" ] });
+        expect(generateTravelPlan( { corn: 0, geese: 1 } )).toMatchObject( { plan: [ "goose" ] });
     })
 
     test('that a single bag of corn returns a travel plan showing a single crossing with the farmer and a bag of corn', () => {
-        expect(generateTravelPlan(1, 0)).toMatchObject( { plan: [ "corn" ] });
+        expect(generateTravelPlan( { corn: 1, geese: 0 } )).toMatchObject( { plan: [ "corn" ] });
+    })
+
+    test('that no commodities to transport returns a travel plan showing no crossings', () => {
+        expect(generateTravelPlan( { corn: 0, geese: 0 } )).toMatchObject( { plan: [] });
     })
 });
