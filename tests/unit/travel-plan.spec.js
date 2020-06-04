4f1c1523d903244408d3e7f033c8a823da0e3236
Will Gibson <Will Gibson>
2020-06-04 11:04:40 +0100

    Add travel plan test for 2 bags of corn

--- tests/unit/travel-plan.spec.js
+++ tests/unit/travel-plan.spec.js
@@ -12,11 +12,15 @@ describe('travel plan', () => {
         expect(generateTravelPlan( { corn: 0, geese: 0 } )).toMatchObject( { plan: [] });
     })
 
-    test('that a single goose returns a travel plan showing a single crossing with the farmer and a goose', () => {
+    test('that a single goose returns a travel plan showing a single crossing with a goose', () => {
         expect(generateTravelPlan( { corn: 0, geese: 1 } )).toMatchObject( { plan: [ "goose" ] });
     })
 
-    test('that a single bag of corn returns a travel plan showing a single crossing with the farmer and a bag of corn', () => {
+    test('that a single bag of corn returns a travel plan showing a single crossing with a bag of corn', () => {
         expect(generateTravelPlan( { corn: 1, geese: 0 } )).toMatchObject( { plan: [ "corn" ] });
+    })
+
+    test('that 2 bags of corn returns a travel plan showing two crossings with a bag of corn and one empty handed return journey', () => {
+        expect(generateTravelPlan( { corn: 1, geese: 0 } )).toMatchObject( { plan: [ "corn", "nothing", "corn" ] });
     })
 });
