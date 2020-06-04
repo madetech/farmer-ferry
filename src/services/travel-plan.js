a5312a973408ef644ac1d2b5f850b2ca45bc4c17
Will Gibson <Will Gibson>
2020-06-04 11:00:06 +0100

    Implement zero commodities travel plan

--- src/services/travel-plan.js
+++ src/services/travel-plan.js
@@ -1,5 +1,5 @@
 export function generateTravelPlan(commodities) {
+    if (commodities.corn < 1 && commodities.geese < 1) return { plan: []}
     if (commodities.corn === 1) return { plan: ["corn"]}
-    
     return { plan: [ "goose" ] };
 }
--- tests/unit/travel-plan.spec.js
+++ tests/unit/travel-plan.spec.js
@@ -8,15 +8,15 @@ import { generateTravelPlan } from '@/services/travel-plan.js';
 
 describe('travel plan', () => {
 
+    test('that no commodities to transport returns a travel plan showing no crossings', () => {
+        expect(generateTravelPlan( { corn: 0, geese: 0 } )).toMatchObject( { plan: [] });
+    })
+
     test('that a single goose returns a travel plan showing a single crossing with the farmer and a goose', () => {
         expect(generateTravelPlan( { corn: 0, geese: 1 } )).toMatchObject( { plan: [ "goose" ] });
     })
 
     test('that a single bag of corn returns a travel plan showing a single crossing with the farmer and a bag of corn', () => {
         expect(generateTravelPlan( { corn: 1, geese: 0 } )).toMatchObject( { plan: [ "corn" ] });
-    })
-
-    test('that no commodities to transport returns a travel plan showing no crossings', () => {
-        expect(generateTravelPlan( { corn: 0, geese: 0 } )).toMatchObject( { plan: [] });
     })
 });
