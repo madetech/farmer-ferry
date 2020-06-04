eff0d5aa4b8094524b89be0fafa54d956b212106
Stu Mackellar <github@nusonic.co.uk>
2020-06-04 16:33:25 +0100

    deploy

---
+++ precache-manifest.36743c9a7e029ffb374e5e47bc903230.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f2cfba837eb5a1ada59",
+    "url": "/farmer-ferry/js/chunk-vendors.d62f6625.js"
+  },
+  {
+    "revision": "5d7e085584f8d23f8a5d",
+    "url": "/farmer-ferry/js/app.82fe871d.js"
+  },
+  {
+    "revision": "deb60d95a11c78bf72b18caf71599b23",
+    "url": "/farmer-ferry/index.html"
+  },
+  {
+    "revision": "5d7e085584f8d23f8a5d",
+    "url": "/farmer-ferry/css/app.d7af0155.css"
+  }
+];
