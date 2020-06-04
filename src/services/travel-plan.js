409206d945ba9c1650d959840843fcf48300a76c
Stu Mackellar <github@nusonic.co.uk>
2020-06-04 16:23:00 +0100

    Inverted notPossible implementation

--- src/services/travel-plan.js
+++ src/services/travel-plan.js
@@ -20,21 +20,16 @@ function commodityPlan(commodity, count) {
     return plan;
 }
 
-function notPossible({ corn, geese, foxes }) {
+export function isPossible({ corn, geese, foxes }) {
 
-    if (foxes > 1) return true;
-    if ((geese === 5) && (foxes === 1)) return true;
-    if ((corn === 0) || (geese === 0)) return false;
+    if (foxes > 1) return false;
+    if ((geese === 5) && (foxes === 1)) return false;
+    if ((corn === 0) || (geese === 0)) return true;
 
     if ((corn === 1 && geese === 1)
         || (corn === 2 && geese === 1)
         || (corn === 1 && geese === 2))
-        return false;
+        return true;
 
-    return true;
-}
-
-export function isPossible(commodities) {
-
-    return !notPossible(commodities);
+    return false;
 }
