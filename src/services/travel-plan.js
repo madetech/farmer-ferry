60e455f2a7c1959756df9b2ae419e399af0afbb2
Stu Mackellar <github@nusonic.co.uk>
2020-06-04 12:44:47 +0100

    Implement travel plan for 2 and 1 combinations.

--- src/services/travel-plan.js
+++ src/services/travel-plan.js
@@ -1,7 +1,9 @@
 export function generateTravelPlan(commodities) {
     if (notPossible(commodities)) return { plan: [], isPossible: false }
-    
-    if (commodities.corn < 1 && commodities.geese < 1) return { plan: [], isPossible: true  }
+
+    if (commodities.corn === 2 && commodities.geese === 1) return { plan: [ "goose", "nothing", "corn", "goose", "corn", "nothing", "goose"] }
+    if (commodities.corn === 1 && commodities.geese === 2) return { plan: [ "corn", "nothing", "goose", "corn", "goose", "nothing", "corn" ] }
+    if (commodities.corn < 1 && commodities.geese < 1) return { plan: [], isPossible: true }
     if (commodities.corn === 1 && commodities.geese === 1) return { plan: ["corn", "nothing", "goose"], isPossible: true }
     if (commodities.corn > 0) return { plan: commodityPlan("corn", commodities.corn), isPossible: true };
     if (commodities.geese > 0) return { plan: commodityPlan("goose", commodities.geese), isPossible: true };
@@ -21,5 +23,12 @@ function commodityPlan(commodity, count) {
 }
 
 function notPossible(commodities) {
-    return ((commodities.corn > 0 && commodities.geese > 1) || (commodities.corn > 1 && commodities.geese > 0));
+    if ((commodities.corn === 0) || (commodities.geese === 0)) return false;
+
+    if ((commodities.corn === 1 && commodities.geese === 1)
+        || (commodities.corn === 2 && commodities.geese === 1)
+        || (commodities.corn === 1 && commodities.geese === 2))
+        return false;
+
+    return true;
 }
--- tests/unit/travel-plan.spec.js
+++ tests/unit/travel-plan.spec.js
@@ -26,12 +26,20 @@ describe('travel plan generation', () => {
         expect(generateTravelPlan( { corn: 2, geese: 0 } )).toMatchObject( { plan: [ "corn", "nothing", "corn" ] });
     })
 
+    test('that 2 bags of corn and 1 goose returns a travel plan showing two crossings with a bag of corn, one crossing with a goose and one empty handed return journey', () => {
+        expect(generateTravelPlan( { corn: 2, geese: 1 } )).toMatchObject( { plan: [ "goose", "nothing", "corn", "goose", "corn", "nothing", "goose" ] });
+    })
+
     test('that 5 bags of corn returns a travel plan showing five crossings with a bag of corn and four empty handed return journeys', () => {
         expect(generateTravelPlan( { corn: 5, geese: 0 } )).toMatchObject( { plan: [ "corn", "nothing", "corn", "nothing", "corn" , "nothing", "corn" , "nothing", "corn" ] });
     })
 
     test('that 1 goose and 1 bag of corn returns a travel plan showing one crossing with a goose, one crossing with a bag of corn, and one empty handed return journey', () => {
         expect(generateTravelPlan( { corn: 1, geese: 1 } )).toMatchObject( { plan: [ "corn", "nothing", "goose" ] });
+    })
+
+    test('that 1 bag of corn and 2 geese returns a travel plan showing two crossings with a bag of corn, one crossing with a goose and one empty handed return journey', () => {
+        expect(generateTravelPlan( { corn: 1, geese: 2 } )).toMatchObject( { plan: [ "corn", "nothing", "goose", "corn", "goose", "nothing", "corn" ] });
     })
 
     test('that 2 geese and 2 bags of corn returns a travel plan showing two crossings with a goose, two crossings with a bag of corn, and th empty handed return journey', () => {
